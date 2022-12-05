4045cfc3d6f8b067b4ec2db021eb4549079418a9
Niko Leskinen <niko.leskinen@fmi.fi>
2022-12-05 14:01:32 +0200

    Add python 3.10 requirement

--- stubs/lark/__init__.pyi
+++ stubs/lark/__init__.pyi
@@ -1,12 +1,21 @@
-from halo_reader.metadata import Metadata
-from halo_reader.variable import Variable
-from halo_reader.attribute import Attribute
-from halo_reader.transformer import HeaderTransformer
-
 from typing import Callable
+
+from halo_reader.attribute import Attribute
+from halo_reader.metadata import Metadata
+from halo_reader.transformer import HeaderTransformer
+from halo_reader.variable import Variable
 
 class Transformer: ...
 
 class Lark:
-    def __init__(self, grammar: str, transformer: HeaderTransformer, parser: str): ...
-    def parse(self, string: str) -> tuple[Metadata, list[Variable], list[Variable], Callable[[Variable, Attribute], Variable]]: ...
+    def __init__(
+        self, grammar: str, transformer: HeaderTransformer, parser: str
+    ): ...
+    def parse(
+        self, string: str
+    ) -> tuple[
+        Metadata,
+        list[Variable],
+        list[Variable],
+        Callable[[Variable, Attribute], Variable],
+    ]: ...
