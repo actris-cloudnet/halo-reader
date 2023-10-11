a5827233ea3a9facb71363bde1082ae8a690c4da
Tuomas Siipola <tuomas.siipola@fmi.fi>
2023-10-11 17:37:35 +0300

    Quick fix for missing background (e.g. Chilbolton 2023-10-09)

--- src/haloreader/halo.py
+++ src/haloreader/halo.py
@@ -144,6 +144,7 @@ class Halo:
                 halobg_sliced.time,
                 halobg_sliced.background,
                 p_amp,
+                self.doppler_velocity,
             )
         )
         signalmask = haloreader.background_correction.threshold_signalmask(
